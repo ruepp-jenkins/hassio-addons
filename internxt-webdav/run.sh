93e3f80475baf1fdf7b40c3a85ecc03256bc011f
MyUncleSam <12820919+MyUncleSam@users.noreply.github.com>
2025-11-23 16:41:08 +0100

    try fix env

--- internxt-webdav/run.sh
+++ internxt-webdav/run.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 CONFIG_PATH=/data/options.json
+
+echo "Reading config from $CONFIG_PATH" >&2
+cat $CONFIG_PATH >&2
 
 export INXT_USER="$(jq -r '.user' $CONFIG_PATH)"
 export INXT_PASSWORD="$(jq -r '.password' $CONFIG_PATH)"
@@ -8,7 +11,5 @@ export INXT_TWOFACTORCODE="$(jq -r '.twofactorcode // empty' $CONFIG_PATH)"
 export INXT_OTPTOKEN="$(jq -r '.otptoken // empty' $CONFIG_PATH)"
 export WEBDAV_PORT="$(jq -r '.webdavport' $CONFIG_PATH)"
 export WEBDAV_PROTOCOL="$(jq -r '.webdavprotocol' $CONFIG_PATH)"
-
-printenv | sort -h
 
 exec /app/docker/entrypoint.sh
