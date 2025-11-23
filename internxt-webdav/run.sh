d2391889f2437e138290faac67fe66c8243d7891
MyUncleSam <12820919+MyUncleSam@users.noreply.github.com>
2025-11-23 16:29:13 +0100

    fix environment variables

---
+++ internxt-webdav/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CONFIG_PATH=/data/options.json
+
+export INXT_USER="$(jq -r '.user' $CONFIG_PATH)"
+export INXT_PASSWORD="$(jq -r '.password' $CONFIG_PATH)"
+export INXT_TWOFACTORCODE="$(jq -r '.twofactorcode // empty' $CONFIG_PATH)"
+export INXT_OTPTOKEN="$(jq -r '.otptoken // empty' $CONFIG_PATH)"
+export WEBDAV_PORT="$(jq -r '.webdavport' $CONFIG_PATH)"
+export WEBDAV_PROTOCOL="$(jq -r '.webdavprotocol' $CONFIG_PATH)"
+
+exec /app/docker/entrypoint.sh
